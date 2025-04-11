72e397f4aa39cf7c3f0445bd7680b179568464aa
varungautam1411 <archervg@gmail.com>
2025-04-11 13:28:22 -0700

    Update WatchedBy.java

--- src/main/java/com/movierecommender/model/input/WatchedBy.java
+++ src/main/java/com/movierecommender/model/input/WatchedBy.java
@@ -1,1 +1,12 @@
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class WatchedBy {
+    @JsonProperty("customer-id")
+    private String customerId;
+    @JsonProperty("movie-id")
+    private String movieId;
+    private int rating;
+    private String date;
+}
 
