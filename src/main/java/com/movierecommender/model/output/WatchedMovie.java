79d805491f2153857d774501ab4cb579d1a06694
varungautam1411 <archervg@gmail.com>
2025-04-11 13:29:06 -0700

    Update WatchedMovie.java

--- src/main/java/com/movierecommender/model/output/WatchedMovie.java
+++ src/main/java/com/movierecommender/model/output/WatchedMovie.java
@@ -1,1 +1,11 @@
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class WatchedMovie {
+    private String movieId;
+    private String title;
+    private int yearOfRelease;
+    private int rating;
+    private String date;
+}
 
