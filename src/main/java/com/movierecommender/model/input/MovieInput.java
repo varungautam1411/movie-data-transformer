d7a2e2312a183a930a367e000f201844e5fbf3bc
varungautam1411 <archervg@gmail.com>
2025-04-11 13:28:04 -0700

    Update MovieInput.java

--- src/main/java/com/movierecommender/model/input/MovieInput.java
+++ src/main/java/com/movierecommender/model/input/MovieInput.java
@@ -1,1 +1,10 @@
-
+// Model classes
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class MovieInput {
+    private List<WatchedBy> watchedBy;
+    private String title;
+    private String movieId;
+    private int yearOfRelease;
+}
