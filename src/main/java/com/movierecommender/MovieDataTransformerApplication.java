dab1506b0ef57742048dfba6cff46f2113ea69f3
varungautam1411 <archervg@gmail.com>
2025-04-11 13:20:27 -0700

    Create MovieDataTransformerApplication.java

---
+++ src/main/java/com/movierecommender/MovieDataTransformerApplication.java
@@ -0,0 +1,7 @@
+// MovieDataTransformer.java
+@SpringBootApplication
+public class MovieDataTransformer {
+    public static void main(String[] args) {
+        SpringApplication.run(MovieDataTransformer.class, args);
+    }
+}
