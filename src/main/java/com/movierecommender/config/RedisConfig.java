8a80ab7340bca9c44fb186ec79c42d3704921dc4
varungautam1411 <archervg@gmail.com>
2025-04-11 13:30:32 -0700

    Update RedisConfig.java

--- src/main/java/com/movierecommender/config/RedisConfig.java
+++ src/main/java/com/movierecommender/config/RedisConfig.java
@@ -1,1 +1,32 @@
+@Configuration
+public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String redisHost;
+    
+    @Value("${spring.redis.port}")
+    private int redisPort;
+    
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+    
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+    
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        config.setPassword(redisPassword);
+        
+        return new LettuceConnectionFactory(config);
+    }
+}
 
+    
