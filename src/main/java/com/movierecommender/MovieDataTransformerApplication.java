48738cb58723af373e0a200ed63c52b8eb7d2a67
varungautam1411 <archervg@gmail.com>
2025-04-11 17:03:38 -0700

    Update MovieDataTransformerApplication.java

--- src/main/java/com/movierecommender/MovieDataTransformerApplication.java
+++ src/main/java/com/movierecommender/MovieDataTransformerApplication.java
@@ -1,4 +1,10 @@
+package com.movierecommender;
+
 // MovieDataTransformer.java
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class MovieDataTransformer {
     public static void main(String[] args) {
