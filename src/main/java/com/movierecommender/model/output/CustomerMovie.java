996e71b7d2899f00bf2b99162d33e6bbcaa2c369
varungautam1411 <archervg@gmail.com>
2025-04-11 17:02:45 -0700

    Update CustomerMovie.java

--- src/main/java/com/movierecommender/model/output/CustomerMovie.java
+++ src/main/java/com/movierecommender/model/output/CustomerMovie.java
@@ -1,7 +1,26 @@
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-class CustomerMovie {
+// src/main/java/com/movierecommender/model/output/CustomerMovie.java
+package com.movierecommender.model.output;
+
+import java.util.List;
+
+public class CustomerMovie {
     private String customerId;
     private List<WatchedMovie> watchedMovies;
+
+    // Getters and Setters
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public List<WatchedMovie> getWatchedMovies() {
+        return watchedMovies;
+    }
+
+    public void setWatchedMovies(List<WatchedMovie> watchedMovies) {
+        this.watchedMovies = watchedMovies;
+    }
 }
