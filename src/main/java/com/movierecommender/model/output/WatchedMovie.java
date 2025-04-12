0b5c13634a7458c82f040c4f189183d1d91b6714
varungautam1411 <archervg@gmail.com>
2025-04-11 17:02:59 -0700

    Update WatchedMovie.java

--- src/main/java/com/movierecommender/model/output/WatchedMovie.java
+++ src/main/java/com/movierecommender/model/output/WatchedMovie.java
@@ -1,11 +1,51 @@
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-class WatchedMovie {
+// src/main/java/com/movierecommender/model/output/WatchedMovie.java
+package com.movierecommender.model.output;
+
+public class WatchedMovie {
     private String movieId;
     private String title;
     private int yearOfRelease;
     private int rating;
     private String date;
-}
 
+    // Getters and Setters
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
