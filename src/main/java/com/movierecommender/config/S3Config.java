c360905cbe7b98bec55f648317b2a406882db51c
varungautam1411 <archervg@gmail.com>
2025-04-11 17:01:25 -0700

    Update S3Config.java

--- src/main/java/com/movierecommender/config/S3Config.java
+++ src/main/java/com/movierecommender/config/S3Config.java
@@ -1,3 +1,11 @@
+// src/main/java/com/movierecommender/config/S3Config.java
+package com.movierecommender.config;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 // Configuration classes
 @Configuration
 public class S3Config {
